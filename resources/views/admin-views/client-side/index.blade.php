3f5db27f68465eed6eade7cc95ad29eeea149edf
officework10 <ow63059@gmail.com>
2025-12-09 22:51:52 -0800

    all side bar page hide jquery link

--- resources/views/admin-views/client-side/index.blade.php
+++ resources/views/admin-views/client-side/index.blade.php
@@ -374,7 +374,7 @@
 
 @push('script_2')
     <script src="{{ asset('public/assets/admin') }}/js/view-pages/client-side-index.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.full.min.js"></script>
 
     <script>
