1669016ce0099f65fca3f5ebdb3fa45b8f7b8bc0
officework10 <ow63059@gmail.com>
2026-02-18 14:38:15 +0500

    bypass activation check

--- app/Traits/ActivationClass.php
+++ app/Traits/ActivationClass.php
@@ -89,26 +89,6 @@ trait ActivationClass
 
     public function checkActivationCache(string|null $app)
     {
-        if ($this->is_local() || is_null($app) || env('DEVELOPMENT_ENVIRONMENT', false)) {
-            return true;
-        }
-
-        $config = $this->getAddonsConfig();
-        $cacheKey = $this->getSystemAddonCacheKey(app: $app);
-
-        if (isset($config[$app])) {
-            if (!isset($config[$app]['active']) || $config[$app]['active'] == 0) {
-                Cache::forget($cacheKey);
-                return false;
-            }
-            $appConfig = $config[$app];
-            return Cache::remember($cacheKey, $this->getCacheTimeoutByDays(days: 1), function () use ($app, $appConfig) {
-                $response = $this->getRequestConfig(username: $appConfig['username'], purchaseKey: $appConfig['purchase_key'], softwareId: $appConfig['software_id'], softwareType: $appConfig['software_type'] ?? base64_decode('cHJvZHVjdA=='));
-                $this->updateActivationConfig(app: $app, response: $response);
-                return (bool)$response['active'];
-            });
-        }
-
         return true;
     }
 
