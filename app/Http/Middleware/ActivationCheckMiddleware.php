6cf480fdca58334f13319d786be009c47ac021cf
officework10 <ow63059@gmail.com>
2026-02-18 14:41:25 +0500

    completely remove activation middleware logic and fix directory casing in git

--- app/Http/Middleware/ActivationCheckMiddleware.php
+++ app/Http/Middleware/ActivationCheckMiddleware.php
@@ -2,16 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use App\Traits\ActivationClass;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-
 
 class ActivationCheckMiddleware
 {
-    use ActivationClass;
-
     /**
      * Handle an incoming request.
      *
@@ -21,18 +16,6 @@ class ActivationCheckMiddleware
      */
     public function handle(Request $request, Closure $next, $area = null): mixed
     {
-        // $response = $this->checkActivationCache(app: $area);
-        $response = true;
-
-        if (!$response) {
-            if (!strpos(url()->current(), '/api')) {
-                return Redirect::away(route(base64_decode('c3lzdGVtLmFjdGl2YXRpb24tY2hlY2s=')))->send();
-            }
-            return response()->json([
-                'code' => 503,
-                'message' => 'Please check activation for '. str_replace('_', ' ', $area),
-            ], 503);
-        }
         return $next($request);
     }
 }
