9ee6d31b35880a825821459c1061a7a921097303
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2026-02-22 14:30:37 +0500

    i have added here

--- database/migrations/2026_02_22_025726_add_discount_amount_to_orders_table.php
+++ database/migrations/2026_02_22_025726_add_discount_amount_to_orders_table.php
@@ -4,25 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+
+            $table->float('discount_amount')
+                ->default(0)
+                ->after('offer_type'); // ✅ After offer_type
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+
+            $table->dropColumn('discount_amount');
+
         });
     }
 };
