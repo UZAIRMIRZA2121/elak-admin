e32eb0e1f9fe67ad2a0fa51882a8ef34959fbebd
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2026-04-01 17:31:10 +0500

    fix: Remove debug statement and clean up order processing logic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -518,8 +518,7 @@ class HomeController extends Controller
                 $order->save();
             }
         }
-        dd($activeOrders);
-
+   
         foreach ($processingOrders as $order) {
 
 
